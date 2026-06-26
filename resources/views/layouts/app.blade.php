908e96a5c9bb88503e79535e2c2defba35c86c92
rifan <syahrifan129@gmail.com>
2026-06-26 09:18:41 +0700

    tambah Tawk.to live chat widget di semua halaman publik

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1512,5 +1512,19 @@
         })();
     });
     </script>
+
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/6a3de0f794471a1d4dea3a69/1js0rdi6g';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
 </body>
 </html>
